f26ba6e9a0457fa31cca8f07ee89bcc6b1519ad7
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2025-10-27 13:52:33 +0900

    Fix validation for logging

--- sched-art/src/bpf/main.bpf.c
+++ sched-art/src/bpf/main.bpf.c
@@ -15,12 +15,16 @@ struct {
 
 static bool is_prior_task(pid_t pid)
 {
-    return bpf_map_lookup_elem(&prior_tasks, &pid);
+    if (bpf_map_lookup_elem(&prior_tasks, &pid) == NULL) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 s32 BPF_STRUCT_OPS(art_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wake_flags) {
     if (is_prior_task(p->pid)) {
-        bpf_printk("[sched-art] Prior task found: %d", p->pid);
+        bpf_printk("[sched-art] Prior task found: %d\n", p->pid);
     }
 
     return prev_cpu;
